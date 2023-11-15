cf3d92cdbf80860dc53496131ed01bd570a3690d
aviral gupta <aviralg2002@gmail.com>
2023-11-15 14:55:32 +0530

    Changed the route response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     
-    res.send("Hello world");
+    res.send("Jai shri ram bhai log");
 })
 
 app.listen(PORT, (req, res) => {
